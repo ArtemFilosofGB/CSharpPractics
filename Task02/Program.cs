bb7650daf7e9bc4b09469a96cae01d2fd59d8adc
ArtemFilosof <afilosof1@gmail.com>
2023-09-19 10:50:18 +0300

    Часть дз готова

---
+++ Task02/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+/*//Проверка на ввод числа
+while (true)
+{
+    Console.Write("Введите 1е число: ");
+    string text1 = Console.ReadLine();
+    Console.Write("Введите 2е число: ");
+    string text2 = Console.ReadLine();
+    if (int.TryParse(text1, out int number1) && int.TryParse(text2, out int number1))
+    {
+        Console.WriteLine("Вы ввели допустимые числа");
+        break;
+    }
+    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+}
+*/
+
+if (number1 == number2)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    if (number1 > number2)
+    {
+        Console.WriteLine("Больше первое число");
+    }
+    else
+    {
+        Console.WriteLine("Больше второе число");
+    }
+}
+//Чтобы исключить ввод стрики в поле числа
+
---
+++ Task04/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое целое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число:");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 && number1 == number3)
+{
+    Console.WriteLine("Вы ввели три одинаковых числа!");
+}
+else
+{
+    int max = number1;
+    if (number2 > number1)
+    {
+        max = number2;
+    }
+    if (number3 > max)
+    {
+        max = number3;
+    }
+
+    Console.WriteLine("Максимальное число :");
+    Console.WriteLine(max);
+}
