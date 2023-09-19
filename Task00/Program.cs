85851ad5c239490c0b7704c47c917538a44d3895
ArtemFilosof <afilosof1@gmail.com>
2023-09-19 11:10:39 +0300

    added task6

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Квадрат числа
+// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Квадрат числа " + number + " = ");
+//Console.Write(number * number);
+int squere = number * number;
+Console.WriteLine($"{number} -> {squere}");
