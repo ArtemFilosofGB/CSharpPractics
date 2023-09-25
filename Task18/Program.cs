d6b14c5563e386bf0514df41a24b55856703e478
ArtemFilosof <afilosof1@gmail.com>
2023-09-25 11:44:17 +0300

    End of seminar 3

---
+++ Task18/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+
+
+int GetUserInput(string str)
+{
+    Console.WriteLine(str);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+string RangeCoordinats(int quater)
+{
+    if (quater == 1) return "x > 0 y > 0";
+    if (quater == 2) return "x < 0 && y > 0";
+    if (quater == 3) return "x < 0 && y < 0";
+    if (quater == 4) return "x > 0 && y < 0";
+
+    return null;
+
+}
+
+int quaterInput = GetUserInput("Ведите четверть:");
+string rangeCoordinats = RangeCoordinats(quaterInput);
+string result = rangeCoordinats != null
+                ? $"Диапазон координат указанной четверти:{rangeCoordinats}"
+                : "Некоректный ввод";
+Console.WriteLine(result);
+
+
---
+++ Task22/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+
+
+int GetUserInput(string str)
+{
+    Console.WriteLine(str);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void SqereNumbers (int num)
+{
+    int count =1;
+    while (count<=num)
+    {
+        Console.WriteLine($"{count,3}->{count*count,5}");
+        count++;
+    }
+
+}
+
+int x = GetUserInput("Введите число");
+SqereNumbers(x);
