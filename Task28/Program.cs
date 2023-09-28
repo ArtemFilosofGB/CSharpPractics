758d1ac40079ca1cde26c1c0fdef49f39ad992f0
ArtemFilosof <afilosof1@gmail.com>
2023-09-28 12:14:56 +0300

    Семинар 4

---
+++ Task28/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+//Факториал
+
+int GetUserInput(string msg)
+{
+    Console.Write($"{msg}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int Factorial(int num)
+{
+    int fac = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked //проверка на переполнение типа
+        {
+            fac *= i;
+        }
+    }
+    return fac;
+}
+
+int number = GetUserInput("Enter a number");
+int fact = Factorial(number);
+
+Console.WriteLine($"Произведение чисел от 1 до {number} -> {fact}");
