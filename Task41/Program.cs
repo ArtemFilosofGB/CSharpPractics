0e274450521fe99de59c3a2a95a9c36b7917b559
ArtemFilosof <afilosof1@gmail.com>
2023-10-05 17:50:28 +0300

    noutebook work 2

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+int GetUserInput(string str)
+{
+    Console.WriteLine(str);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int number = GetUserInput("Imput numbers. For exit push -q- button");
+bool isRuning = true;
+while (isRuning)
+{
+    ConsoleKeyInfo keyInfo = GetImput();
+    if (Console.ReadLine)
+    {
+        
+    }
+    
+}
