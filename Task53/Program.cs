ab1ee492d8d280ab99a18916ae6e20c2b78a019a
ArtemFilosof <afilosof1@gmail.com>
2023-10-12 12:24:52 +0300

    Семинар 8

---
+++ Task53/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++) // matrix.GetLength(0)
+    {
+        for (int j = 0; j < colums; j++) // matrix.GetLength(1)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+            //matrix[i,j] = i+j;  
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrixInt(int[,] arr) //Метод, который выводит массив
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],5} "); // формат вывода в 5 символов
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void SwapMatrixFirstLastRow(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+
+int[,] rndMatrix = CreateMatrixRndInt(4,5,0,10);
+PrintMatrixInt(rndMatrix);
+SwapMatrixFirstLastRow(rndMatrix);
+Console.WriteLine();
+PrintMatrixInt(rndMatrix);
---
+++ Task59/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел.
+// Напишите программу, которая удалит строку и столбец, на
+// пересечении которых расположен наименьший элемент
+// массива.
+
+int[,] CreateMatrix(int rows, int columns, int minValue, int maxValue) //Метод, который создает двумерный массив
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(minValue, maxValue);
+
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix) //Метод, который выводит двумерный массив в консоль
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[] FindLowerValueRowCol(int[,] matrix)
+{
+    int rowNum = 0;
+    int colNum = 0;
+    int[] res = new int[2];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < matrix[rowNum, colNum])
+            {
+                rowNum = i;
+                colNum = j;
+            }
+
+        }
+    }
+    return new int[] { rowNum, colNum };
+
+}
+
+int[,] MatrixRowColDelByCoords(int[,] matrix, int rowTodel, int colTodel)
+{
+    int[,] resMatrix = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+
+    int row = 0;
+    for (int i = 0; i < resMatrix.GetLength(0); i++)
+    {
+        if (i == rowTodel) row++;
+        int col = 0;
+        for (int j = 0; j < resMatrix.GetLength(1); j++)
+        {
+            if (j == colTodel) col++;
+            resMatrix[i, j] = matrix[row, col];
+            col++;
+        }
+        row++;
+    }
+    return resMatrix;
+}
+
+int[,] customMatrix = CreateMatrix(4, 4, 0, 10);
+PrintMatrix(customMatrix);
+Console.WriteLine();
+int[] coords = FindLowerValueRowCol(customMatrix);
+Console.WriteLine($"row {coords[0]} , {coords[1]} min val = {customMatrix[coords[0], coords[1]]}");
+int[,] resultArr = MatrixRowColDelByCoords(customMatrix,coords[0], coords[1]);
+PrintMatrix(resultArr);
+
