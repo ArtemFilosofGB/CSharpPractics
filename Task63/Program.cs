ed76e018616ea5f5f68af4e29fa9dfb9cd257a11
AFilosof <afilosof1@gmail.com>
2023-10-18 23:09:20 +0300

    Task63

--- Task63/Program.cs
+++ Task63/Program.cs
@@ -3,21 +3,21 @@
 //N = 5 -> "1, 2, 3, 4, 5"
 //N = 6 -> "1, 2, 3, 4, 5, 6"
 
-
 Console.WriteLine("Введите натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if(number < 1)
+if (number < 1)
 {
-   Console.WriteLine("Некорректный ввод!");
-   return;
+    Console.WriteLine("Некорректный ввод!");
+    return;
 }
 
 NaturalsNumbers(number);
 
-void NaturalsNumbers(int num)
-{   int i = num-1;
+void NaturalsNumbers(int num, int count = 1)
+{
+    int i = num - count;
     if (num == 0) return;
-    Console.Write($"{num-i} ");
-    NaturalsNumbers(num - 1);
+    Console.Write($"{num - i} ");
+    NaturalsNumbers(num - 1, count+1);
 }
